e1ea683b1f7fa6b60e6748aeaf00d7f047f53952
Alexey Kim <kim805316@gmail.com>
2023-01-12 16:23:06 +0300

    count1 added

--- new2_1.py
+++ new2_1.py
@@ -1,6 +1,8 @@
 strings = int(input('Введите желаемое количество строк: '))
 count = 0
+count1 = 1
 while count < strings:
   print('Я - программист!')
   count += 1
+  count1 += 1
   print('строка')
